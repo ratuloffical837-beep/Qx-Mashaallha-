b98d563656c3a0b0c9fae817cdc96d47f0dd4739
ratuloffical837-beep <ratuloffical837@gmail.com>
2026-01-07 19:41:13 +0600

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
+
+# পাইপ (pip) আপডেট করা
 pip install --upgrade pip
+
+# আপনার requirements.txt থেকে সব লাইব্রেরি ইন্সটল করা
 pip install -r requirements.txt
+
+# রেন্ডারে ব্রাউজার (Chromium) ইন্সটল করার সঠিক কমান্ড
+# এখানে 'install-deps' ব্যবহার করা হবে না কারণ রেন্ডারে রুট এক্সেস নেই
 playwright install chromium
-playwright install-deps chromium
