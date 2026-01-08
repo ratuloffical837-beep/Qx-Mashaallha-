64923b9b2c432dfb54ff4b4440a8bb6a3259a72f
ratuloffical837-beep <ratuloffical837@gmail.com>
2026-01-08 18:59:31 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env
+    'process.env': {}
   },
   server: { host: true, port: 3000 }
 })
