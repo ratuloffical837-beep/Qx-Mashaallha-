f7735dbbb2efae84b80219d256e95ccbe37615ea
ratuloffical837-beep <ratuloffical837@gmail.com>
2026-01-09 00:15:51 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ 
+
+export default defineConfig({
   plugins: [react()],
-  server: { host: true, port: 3000 }
+  server: { 
+    host: true, 
+    port: 3000 
+  }
 })
